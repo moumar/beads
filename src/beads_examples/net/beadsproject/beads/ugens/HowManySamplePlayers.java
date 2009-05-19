1ff99eb889a492dba83a3fc35989f2027963576a
orsjb <orsjb@2ebb4c82-e849-0410-bb35-b480c621eb2c>
2009-05-19 05:49:51 +0000

    Added HowManySamplePlayers in examples. Cleaned up src dir which still had tests dir inside it.

--- src/beads_examples/net/beadsproject/beads/ugens/HowManySamplePlayers.java
+++ src/beads_examples/net/beadsproject/beads/ugens/HowManySamplePlayers.java
@@ -1,22 +1,25 @@
 package net.beadsproject.beads.ugens;
 
 import net.beadsproject.beads.core.AudioContext;
-import net.beadsproject.beads.data.buffers.SineBuffer;
+import net.beadsproject.beads.data.Sample;
+import net.beadsproject.beads.data.SampleManager;
 
-
-public class HowManyWavePlayers {
+public class HowManySamplePlayers {
 
 	//Try and thrash your system
-	public final static int NUM_OSCS = 800;
+	public final static int NUM_OSCS = 100;
 	public final static int BUFFER_SIZE = 512;
 	public final static int IO_BUFFER_SIZE = 1024;
 	
 	public static void main(String[] args) {
-		final AudioContext ac = new AudioContext(BUFFER_SIZE, IO_BUFFER_SIZE * 4);
+		final AudioContext ac = new AudioContext();
+//		final AudioContext ac = new AudioContext(BUFFER_SIZE, IO_BUFFER_SIZE * 4);
 		ac.chooseMixerCommandLine();
+		Sample s = SampleManager.sample("audio/1234.aif");
 		final Gain g = new Gain(ac, 2, 0.5f / NUM_OSCS);
 		for(int i = 0; i < NUM_OSCS; i++) {
-			WavePlayer wp = new WavePlayer(ac, (float)Math.random() * 5000f + 100f, new SineBuffer().getDefault());
+			SamplePlayer wp = new SamplePlayer(ac, s);
+			wp.getRateEnvelope().setValue((float)Math.random() + 0.5f);
 			g.addInput(wp);
 		}
 		ac.out.addInput(g);
