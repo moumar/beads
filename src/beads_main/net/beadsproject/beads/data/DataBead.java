a9724f2697613b547f56e029e2afea294142cda1
benito <benito@2ebb4c82-e849-0410-bb35-b480c621eb2c>
2010-01-24 19:14:12 +0000

    added two new convenience methods:
    getFloatArray and getUGenArray

--- src/beads_main/net/beadsproject/beads/data/DataBead.java
+++ src/beads_main/net/beadsproject/beads/data/DataBead.java
@@ -8,7 +8,7 @@ import java.util.*;
  * values may be any Object. Implements the Map interface.
  * 
  * @author Benito Crawford
- * @version 0.9
+ * @version 0.9.5
  */
 public class DataBead extends Bead implements Map<String, Object> {
 	public Hashtable<String, Object> properties;
@@ -129,6 +129,107 @@ public class DataBead extends Bead implements Map<String, Object> {
 		Object o = get(key);
 		if (o instanceof UGen) {
 			return (UGen) o;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Gets a float array from the value stored with the specified key. If the
+	 * stored value is actually of type <code>float[]</code>, the method returns
+	 * that object. In the event that the stored value is an array of numbers of
+	 * some other type, the method will return a new float array filled with
+	 * values converted to float; an array of doubles, for instance, will be
+	 * recast as floats. Single numbers will be returned as a one-element float
+	 * array. If no array can be formed from the stored value, or if there is no
+	 * stored value, the method returns <code>null</code>.
+	 * 
+	 * @param key
+	 *            The key.
+	 * @return The derived float array.
+	 */
+	public float[] getFloatArray(String key) {
+		Object o = get(key);
+		float[] ret;
+		if (o instanceof Number[]) {
+			Number[] n = (Number[]) o;
+			ret = new float[n.length];
+			for (int i = 0; i < n.length; i++) {
+				ret[i] = n[i].floatValue();
+			}
+		} else if (o instanceof float[]) {
+			ret = (float[]) o;
+		} else if (o instanceof double[]) {
+			double[] p = (double[]) o;
+			ret = new float[p.length];
+			for (int i = 0; i < p.length; i++) {
+				ret[i] = (float) p[i];
+			}
+		} else if (o instanceof int[]) {
+			int[] p = (int[]) o;
+			ret = new float[p.length];
+			for (int i = 0; i < p.length; i++) {
+				ret[i] = (float) p[i];
+			}
+		} else if (o instanceof long[]) {
+			long[] p = (long[]) o;
+			ret = new float[p.length];
+			for (int i = 0; i < p.length; i++) {
+				ret[i] = (float) p[i];
+			}
+		} else if (o instanceof char[]) {
+			char[] p = (char[]) o;
+			ret = new float[p.length];
+			for (int i = 0; i < p.length; i++) {
+				ret[i] = (float) p[i];
+			}
+		} else if (o instanceof byte[]) {
+			byte[] p = (byte[]) o;
+			ret = new float[p.length];
+			for (int i = 0; i < p.length; i++) {
+				ret[i] = (float) p[i];
+			}
+		} else if (o instanceof short[]) {
+			short[] p = (short[]) o;
+			ret = new float[p.length];
+			for (int i = 0; i < p.length; i++) {
+				ret[i] = (float) p[i];
+			}
+		} else if (o instanceof boolean[]) {
+			boolean[] p = (boolean[]) o;
+			ret = new float[p.length];
+			for (int i = 0; i < p.length; i++) {
+				if (p[i] == true) {
+					ret[i] = 1;
+				} else {
+					ret[i] = 0;
+				}
+			}
+		} else if (o instanceof Number) {
+			ret = new float[] { ((Number) o).floatValue() };
+		} else {
+			ret = null;
+		}
+
+		return ret;
+	}
+
+	/**
+	 * Gets an array of UGens from the value stored with the specified key. If
+	 * the value is a UGen object (not an array), the method returns a new
+	 * one-element UGen array with the value stored in it. If the value is
+	 * empty, or not a UGen array or UGen, the method returns <code>null</code>.
+	 * 
+	 * @param key
+	 *            The key.
+	 * @return The UGen array.
+	 */
+	public UGen[] getUGenArray(String key) {
+		Object o = get(key);
+		if (o instanceof UGen[]) {
+			return (UGen[]) o;
+		} else if (o instanceof UGen) {
+			return new UGen[] { (UGen) o };
 		} else {
 			return null;
 		}
