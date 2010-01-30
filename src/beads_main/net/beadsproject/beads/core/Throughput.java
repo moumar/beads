07263b1e2c45880b15e4d4b43985241aadac270a
benito <benito@2ebb4c82-e849-0410-bb35-b480c621eb2c>
2010-01-30 18:11:08 +0000

    Throughput - even simpler than a gain - useful for grabbing buffers

---
+++ src/beads_main/net/beadsproject/beads/core/Throughput.java
@@ -0,0 +1,42 @@
+package net.beadsproject.beads.core;
+
+/**
+ * A simple UGen that just forwards its inputs to its outputs.
+ * 
+ * @author Benito Crawford
+ * @version 0.9.5
+ * 
+ */
+public class Throughput extends UGen {
+
+	/**
+	 * Constructor for a one-channel Throughput using the specified audio
+	 * context.
+	 * 
+	 * @param context
+	 *            The audio context.
+	 */
+	public Throughput(AudioContext context) {
+		this(context, 1);
+	}
+
+	/**
+	 * Constructor for a Throughput with the specified number of channels, using
+	 * the specified audio context.
+	 * 
+	 * @param context
+	 *            The audio context.
+	 * @param channels
+	 *            The number of channels.
+	 */
+	public Throughput(AudioContext context, int channels) {
+		super(context, channels, channels);
+		this.outputInitializationRegime = OutputInitializationRegime.RETAIN;
+		bufOut = bufIn;
+	}
+
+	@Override
+	public void calculateBuffer() {
+	}
+
+}
