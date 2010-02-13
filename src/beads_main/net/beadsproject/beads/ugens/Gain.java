4b21f5927994b2f6d5fad2c3b5bb47d35c2b88a2
benito <benito@2ebb4c82-e849-0410-bb35-b480c621eb2c>
2010-02-13 23:51:24 +0000

    renamed methods, optimized slightly
    - kept old methods as deprecated.

--- src/beads_main/net/beadsproject/beads/ugens/Gain.java
+++ src/beads_main/net/beadsproject/beads/ugens/Gain.java
@@ -3,25 +3,24 @@
  */
 package net.beadsproject.beads.ugens;
 
-import java.util.Arrays;
 import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.core.UGen;
-import net.beadsproject.beads.data.Buffer;
-import net.beadsproject.beads.data.buffers.Log01Buffer;
+import net.beadsproject.beads.data.*;
 
 /**
- * Gain modifies the gain of a multi-channel audio signal. The gain value 
- * can be controlled by an audio signal.
- *
+ * Gain modifies the gain of a multi-channel audio signal. The gain value can be
+ * controlled by an audio signal.
+ * 
  * @beads.category effect
  * @author ollie
  */
-public class Gain extends UGen {
+public class Gain extends UGen implements DataBeadReceiver {
 
-    /** The gain envelope. */
-    private UGen gainEnvelope;
-    
-    /**
+	/** The gain envelope. */
+	private UGen gainUGen;
+	private float gain = 1;
+
+	/**
 	 * Instantiates a new Gain.
 	 * 
 	 * @param context
@@ -31,13 +30,14 @@ public class Gain extends UGen {
 	 * @param gainEnvelope
 	 *            the gain envelope.
 	 */
-    public Gain(AudioContext context, int inouts, UGen gainEnvelope) {
-       super(context, inouts, inouts);
-        setGainEnvelope(gainEnvelope);
-    }
-    
-    /**
-	 * Instantiates a new Gain with a {@link Static} gain envelop with the given value.
+	public Gain(AudioContext context, int inouts, UGen gainEnvelope) {
+		super(context, inouts, inouts);
+		setGain(gainEnvelope);
+	}
+
+	/**
+	 * Instantiates a new Gain with a {@link Static} gain envelop with the given
+	 * value.
 	 * 
 	 * @param context
 	 *            the AudioContext.
@@ -46,11 +46,12 @@ public class Gain extends UGen {
 	 * @param gain
 	 *            the fixed gain level.
 	 */
-    public Gain(AudioContext context, int inouts, float gain) {
-       this(context, inouts, new Static(context, gain));
-    }
-    
-    /**
+	public Gain(AudioContext context, int inouts, float gain) {
+		super(context, inouts, inouts);
+		setGain(gain);
+	}
+
+	/**
 	 * Instantiates a new Gain with {@link Static} gain envelop set to 1.
 	 * 
 	 * @param context
@@ -58,41 +59,119 @@ public class Gain extends UGen {
 	 * @param inouts
 	 *            the number of inputs (= number of outputs).
 	 */
-    public Gain(AudioContext context, int inouts) {
-        this(context, inouts, 1f);
-    }
-    
-    /**
+	public Gain(AudioContext context, int inouts) {
+		this(context, inouts, 1f);
+	}
+
+	/**
 	 * Gets the gain envelope.
 	 * 
 	 * @return the gain envelope.
+	 * @deprecated As of version 1.0, replaced by {@link #setGain(UGen)}.
 	 */
-    public UGen getGainEnvelope() {
-        return gainEnvelope;
-    }
-    
-    /**
+	@Deprecated
+	public UGen getGainEnvelope() {
+		return gainUGen;
+	}
+
+	/**
 	 * Sets the gain envelope.
 	 * 
 	 * @param gainEnvelope
 	 *            the new gain envelope.
+	 * @deprecated As of version 1.0, replaced by {@link #setGain(UGen)}.
 	 */
-    public void setGainEnvelope(UGen gainEnvelope) {
-        this.gainEnvelope = gainEnvelope;
-    }
-    
-	/* (non-Javadoc)
-     * @see com.olliebown.beads.core.UGen#calculateBuffer()
-     */
-    @Override
-    public void calculateBuffer() {
-    	gainEnvelope.update();
-        for(int i = 0; i < bufferSize; ++i) {
-            float gain = gainEnvelope.getValue(0, i);
-            for(int j = 0; j < ins; j++) {
-                bufOut[j][i] = gain * bufIn[j][i];
-            }
-        }
-    }
+	@Deprecated
+	public void setGainEnvelope(UGen gainEnvelope) {
+		this.gainUGen = gainEnvelope;
+	}
+
+	/**
+	 * Gets the current gain value.
+	 * 
+	 * @return The gain value.
+	 */
+	public float getGain() {
+		return gain;
+	}
+
+	/**
+	 * Sets the gain to a static float value.
+	 * 
+	 * @param gain
+	 *            The gain value.
+	 * @return This gain instance.
+	 */
+	public Gain setGain(float gain) {
+		this.gainUGen = null;
+		this.gain = gain;
+		return this;
+	}
+
+	/**
+	 * Sets a UGen to control the gain amount.
+	 * 
+	 * @param gainUGen
+	 *            The gain UGen.
+	 * @return This gain instance.
+	 */
+	public Gain setGain(UGen gainUGen) {
+		if (gainUGen == null) {
+			setGain(gain);
+		} else {
+			this.gainUGen = gainUGen;
+			gainUGen.update();
+			gain = gainUGen.getValue();
+		}
+		return this;
+	}
+
+	/**
+	 * Gets the gain UGen, if it exists.
+	 * 
+	 * @return The gain UGen.
+	 */
+	public UGen getGainUGen() {
+		return gainUGen;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.olliebown.beads.core.UGen#calculateBuffer()
+	 */
+	@Override
+	public void calculateBuffer() {
+		if (gainUGen == null) {
+			for (int channel = 0; channel < ins; channel++) {
+				float[] bi = bufIn[channel];
+				float[] bo = bufOut[channel];
+
+				for (int i = 0; i < bufferSize; ++i) {
+					bo[i] = gain * bi[i];
+				}
+			}
+		} else {
+			gainUGen.update();
+			for (int i = 0; i < bufferSize; ++i) {
+				gain = gainUGen.getValue(0, i);
+				for (int channel = 0; channel < ins; channel++) {
+					bufOut[channel][i] = gain * bufIn[channel][i];
+				}
+			}
+		}
+	}
+
+	public DataBeadReceiver sendData(DataBead db) {
+		if (db != null) {
+			UGen u = db.getUGen("gain");
+			if (u == null) {
+				setGain(db.getFloat("gain", gain));
+			} else {
+				setGain(u);
+			}
+		}
+		return this;
+	}
 
 }
