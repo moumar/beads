766ec92c150bb080a43779d37f832b0ca7cc6566
benito <benito@2ebb4c82-e849-0410-bb35-b480c621eb2c>
2010-03-26 02:38:40 +0000

    UsingClock & UsingDelayTrigger - filled in basic examples

--- src/beads_examples/net/beadsproject/beads/ugens/UsingDelayTrigger.java
+++ src/beads_examples/net/beadsproject/beads/ugens/UsingDelayTrigger.java
@@ -7,13 +7,24 @@ public class UsingDelayTrigger {
 
 	public static class Basic {
 		public static void main(String[] args) {
+
+			// Create our audio context.
 			AudioContext ac = new AudioContext();
+
+			// Create a DelayTrigger that sends a message to a new Bead after 5
+			// seconds. That new Bead will then output to the console.
 			DelayTrigger dt = new DelayTrigger(ac, 5000, new Bead() {
 				public void messageReceived(Bead message) {
 					System.out.println("message received");
 				}
 			});
+
+			// Important! Adding the DelayTrigger as a dependent will make sure
+			// that it gets updated - otherwise it won't know that time is
+			// passing and won't fire.
 			ac.out.addDependent(dt);
+
+			// Start processing audio.
 			ac.start();
 		}
 	}
