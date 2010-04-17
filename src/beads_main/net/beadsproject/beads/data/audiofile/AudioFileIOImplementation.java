6076435b3d7a89a9aaa65ba57d119dbfb5cd1a53
orsjb <orsjb@2ebb4c82-e849-0410-bb35-b480c621eb2c>
2010-04-17 13:36:53 +0000

    Docs on new classes, AudioFileIOImplementation and JavaSoundAudioFileIOImplementation. Boy that's a long class name.

--- src/beads_main/net/beadsproject/beads/data/audiofile/AudioFileIOImplementation.java
+++ src/beads_main/net/beadsproject/beads/data/audiofile/AudioFileIOImplementation.java
@@ -4,21 +4,30 @@ import java.io.IOException;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * The Class AudioFileIOImplementation provides methods for how the system
+ * grabs an {@link AudioFile} from a file name or an {@link InputStream}. The
+ * default is {@link JavaSoundAudioFileIOImplementation}.
+ */
 public abstract class AudioFileIOImplementation {
 	
 	/**
-	 * Retrieve an audio file. 
-	 * 
-	 * @param filename
-	 * @return
+	 * Retrieve an audio file from a given filename String.
+	 *
+	 * @param filename the filename.
+	 * @return the audio file
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws UnsupportedAudioFileException an unsupported audio file exception.
 	 */
 	public abstract AudioFile getAudioFile(String filename) throws IOException, UnsupportedAudioFileException;
 	
 	/**
-	 * Retrieve an audio file. 
-	 * 
-	 * @param input stream
-	 * @return
+	 * Retrieve an audio file from a given InputStream.
+	 *
+	 * @param is the InputStream.
+	 * @return the audio file
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws UnsupportedAudioFileException an unsupported audio file exception.
 	 */
 	public abstract AudioFile getAudioFile(java.io.InputStream is) throws IOException, UnsupportedAudioFileException;
 
--- src/beads_main/net/beadsproject/beads/data/audiofile/JavaSoundAudioFileIOImplementation.java
+++ src/beads_main/net/beadsproject/beads/data/audiofile/JavaSoundAudioFileIOImplementation.java
@@ -5,13 +5,23 @@ import java.io.InputStream;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * The Class JavaSoundAudioFileIOImplementation is the default {@link AudioFileIOImplementation}
+ * and returns a {@link JavaSoundAudioFile}.
+ */
 public class JavaSoundAudioFileIOImplementation extends AudioFileIOImplementation {
 	
+	/* (non-Javadoc)
+	 * @see net.beadsproject.beads.data.audiofile.AudioFileIOImplementation#getAudioFile(java.lang.String)
+	 */
 	@Override
 	public AudioFile getAudioFile(String filename) throws IOException, UnsupportedAudioFileException {
 		return new JavaSoundAudioFile(filename);
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.beadsproject.beads.data.audiofile.AudioFileIOImplementation#getAudioFile(java.io.InputStream)
+	 */
 	@Override
 	public AudioFile getAudioFile(InputStream is) throws IOException, UnsupportedAudioFileException {
 		return new JavaSoundAudioFile(is);
